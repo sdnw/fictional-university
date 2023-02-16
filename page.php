0638c67f62e03fb55a605c4df3938974ea1357f2
Steven Dawn <stevendawn98@gmail.com>
2023-02-16 14:05:26 -0500

    added prettier

--- page.php
+++ page.php
@@ -2,13 +2,15 @@
 
 get_header();
 
-    while(have_posts()) {
-        the_post(); ?>
+while (have_posts()) {
+  the_post(); ?>
         
         <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
+        "images/ocean.jpg"
+      ); ?>);"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Don't forget to replace me later</p>
         </div>
@@ -17,12 +19,16 @@ get_header();
 
     <div class="container container--narrow page-section">
 
-    <?php 
+    <?php
     $theParent = wp_get_post_parent_id(get_the_ID());
     if ($theParent) { ?>
          <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php the_title() ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink(
+            $theParent
+          ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(
+  $theParent
+); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
     <?php }
@@ -38,11 +44,12 @@ get_header();
         -->
 
       <div class="generic-content">
-        <?php the_content() ?>
+        <?php the_content(); ?>
       </div>
     </div>
 
-        <?php }
+        <?php
+}
 
 get_footer();
 ?>
