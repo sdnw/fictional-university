1fe4d2c74d21b4574cee74464098684c18e7f71d
Steven Dawn <stevendawn98@gmail.com>
2023-02-21 12:12:48 -0500

    created past events page integrated highlight functionality in header/custom pagination

--- page-past-events.php
+++ page-past-events.php
@@ -9,17 +9,35 @@
         "images/ocean.jpg"
       ); ?>);"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-          <p>See what is going on in our world.</p>
+          <p>A recap of our past events.</p>
         </div>
       </div>
     </div>
 
     <div class="container container--narrow page-section">
       <?php
-      while(have_posts()) {
-        the_post(); ?>
+
+        $today = date('Ymd');
+        $pastEvents = new WP_Query(array(
+            'paged' => get_query_var('paged', 1),
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '<',
+                    'value' => $today,
+                    'type' => 'numeric'
+                )
+            )
+        ));
+
+      while($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
         <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
             <!-- custom field for upcoming events used to display month only -->
@@ -36,12 +54,12 @@
             </div>
           </div>
       <?php }
-      echo paginate_links();
+      // custom query pagination
+      echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+      ));
       ?>
 
-        <hr class="section-break">
-
-      <p>Looking for a recap of all events? <a href="<?php echo site_url('/past-events')?>">Check out our past events archive</a>.</p>
 
   <?php get_footer();
 
