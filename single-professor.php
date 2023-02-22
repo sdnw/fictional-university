c8f91fc9a58f5286233637b7e4b4484308949fc7
Steven Dawn <stevendawn98@gmail.com>
2023-02-22 16:23:33 -0500

    refactored code in pageBanner to be reused throughout app giving different images/subtitles updated readme for university post-types in mu-plugins

--- single-professor.php
+++ single-professor.php
@@ -1,22 +1,14 @@
 <!-- this name is important -->
 <?php
+// call these functions from functions.php
 get_header();
-
 while (have_posts()) {
-  the_post(); ?>
 
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php
-      $pageBannerImage = get_field("page_banner_background_image");
-      echo $pageBannerImage["sizes"]["pageBanner"];
-      ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p><?php the_field("page_banner_subtitle"); ?></p>
-        </div>
-      </div>
-    </div>
+  the_post();
+  pageBanner();
+  ?>
+
+    
 
 <!-- resize the image and have the text next to it two-column style -->
     <div class="container container--narrow page-section">
