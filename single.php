640f36506950ffc1a74148bc3880434d0928a000
Steven Dawn <stevendawn98@gmail.com>
2023-02-24 08:59:54 -0500

    updated reusable pageBanner code in template files

--- single.php
+++ single.php
@@ -2,21 +2,13 @@
 
 get_header();
 
+/* we don't need to fill in pageBanner() because we don't need to pass in any arguments
+ like we did in archive.php */
 while (have_posts()) {
-  the_post(); ?>
 
-         <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
-        "images/ocean.jpg"
-      ); ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Don't forget to replace me later</p>
-        </div>
-      </div>
-    </div>
-
+  the_post();
+  pageBanner();
+  ?>
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
