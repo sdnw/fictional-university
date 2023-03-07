59b9dbf61ab56c438c646df4920722d5ec2da50a
Steven Dawn <stevendawn98@gmail.com>
2023-03-07 09:47:20 -0500

    fixed duplicate campus search

--- inc/search-route.php
+++ inc/search-route.php
@@ -142,6 +142,9 @@ function universitySearchResults($data)
     $results["events"] = array_values(
       array_unique($results["events"], SORT_REGULAR)
     );
+    $results["campuses"] = array_values(
+      array_unique($results["campuses"], SORT_REGULAR)
+    );
   }
 
   return $results;
