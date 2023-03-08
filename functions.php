81368c525a3d253c843d410247379b4478589d87
Steven Dawn <stevendawn98@gmail.com>
2023-03-08 16:17:08 -0500

    customized login/signup form

--- functions.php
+++ functions.php
@@ -159,6 +159,35 @@ function noSubsAdminBar() {
     show_admin_bar(false);
   }
 }
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style(
+    "custom-google-fonts",
+    "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i"
+  );
+  wp_enqueue_style(
+    "font-awesome",
+    "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  );
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLofinTitle() {
+  return get_bloginfo('name');
+}
+
 // add_action("wp_head", "show_template");
 // function show_template()
 // {
