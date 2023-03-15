96e0a9047060f53cab09b4fa94b7204fbcf905bb
Steven Dawn <stevendawn98@gmail.com>
2023-03-15 15:19:48 -0400

    added layer of security to sanitize text in title/content for notes

--- functions.php
+++ functions.php
@@ -192,6 +192,11 @@ function ourLoginTitle() {
 add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
 function makeNotePrivate($data) {
+  if($data['post_type'] == 'note'){
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
+  
   if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
     $data['post_status'] = 'private';
   }
